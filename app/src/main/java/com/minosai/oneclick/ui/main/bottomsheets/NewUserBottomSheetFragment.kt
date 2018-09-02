b3ec0eb56c3eb01c38a27b913a8301ab9612024b
Yaswant Narayan <yoyaswant@gmail.com>
2018-09-02 12:31:24 +0530

    add bottom sheets

--- app/src/main/java/com/minosai/oneclick/ui/main/bottomsheets/NewUserBottomSheetFragment.kt
+++ app/src/main/java/com/minosai/oneclick/ui/main/bottomsheets/NewUserBottomSheetFragment.kt
@@ -2,9 +2,11 @@ package com.minosai.oneclick.ui.main.bottomsheets
 
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialogFragment
+import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.minosai.oneclick.R
 import com.minosai.oneclick.util.listener.NewUserSheetListener
 import kotlinx.android.synthetic.main.fragment_bottom_sheet_newuser.*
@@ -15,9 +17,11 @@ class NewUserBottomSheetFragment() : BottomSheetDialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dialog.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         return inflater.inflate(R.layout.fragment_bottom_sheet_newuser, container, false)
     }
 
